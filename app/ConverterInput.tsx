5935cf56b41cbd5ab3b3c3997008370b5d005ffa
Dhrumi Shah <84569241+dhrumishah@users.noreply.github.com>
2023-05-29 14:10:46 +0530

    dropdown

--- app/ConverterInput.tsx
+++ app/ConverterInput.tsx
@@ -3,19 +3,51 @@ import React, { useState } from "react";
 import Typography from "@mui/material/Typography";
 import Stack from "@mui/material/Stack";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import ReactFlagsSelect from "react-flags-select";
 import "./ConverterInput.css";
-import DropDown from "./DropDown";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+
 type Props = {
   label: string;
 };
 
 const ConverterInput = (props: Props) => {
   const [selected, setSelected] = useState<any>("");
+  const [fromCurrency, setFromCurrency] = useState("INR");
+  const [toCurrency, setToCurrency] = useState("INR");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setToCurrency(event.target.value as string);
+  };
   const handleSelect = (countryCode: any) => {
     setSelected(countryCode);
+  };
+  const handleCurrency = async () => {
+    try {
+      const myHeaders = new Headers();
+      myHeaders.append("apikey", "qGMMLTHEtOwOsv1NPW5Toh5zgKlBLPfu");
+
+      const requestOptions: RequestInit = {
+        method: "GET",
+        redirect: "follow",
+        headers: myHeaders,
+      };
+
+      const response = await fetch(
+        `https://api.apilayer.com/fixer/latest?symbols=${toCurrency}&base=${fromCurrency}`,
+        requestOptions
+      );
+
+      if (!response.ok) {
+        throw new Error("Request failed with status: " + response.status);
+      }
+
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.log("Error:", error);
+    }
   };
   return (
     <Box
@@ -39,25 +71,24 @@ const ConverterInput = (props: Props) => {
           />
         </Stack>
         <Stack direction="column" flexWrap="wrap">
-          {/* <Typography sx={{ color: "gray", mb: 1 }}>
-            Selected Country: {selected}
-          </Typography>
-          <ReactFlagsSelect
-            countries={["US", "GB", "HK", "AU"]}
-            // customLabels={{
-            //   US: "USD",
-            //   GB: "GBP",
-            //   HK: "HKD",
-            //   AU: "AUD",
-            // }}
-            placeholder="Select Curreny"
-            showSelectedLabel={false}
-            showOptionLabel={false}
-            selectedSize={14}
-            selected={selected}
-            onSelect={handleSelect}
-          /> */}
-          <DropDown />
+          <div>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={toCurrency}
+              label="Currency"
+              onChange={handleChange}
+              sx={{
+                backgroundColor: "#EFF4FE",
+                color: "#294FE2",
+                fontWeight: "700",
+              }}
+            >
+              <MenuItem value="USD">USD</MenuItem>
+              <MenuItem value="INR">INR</MenuItem>
+              <MenuItem value="HKD">HKD</MenuItem>
+            </Select>
+          </div>
         </Stack>
       </Stack>
     </Box>
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,65 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import ConverterInput from "./ConverterInput";
 import { Grid, Stack } from "@mui/material";
 import Arrow from "../public/arrow.png";
 import Image from "next/image";
 import Button from "@mui/material/Button";
-import { Roboto } from "next/font/google";
 import "./page.css";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import "./ConverterInput.css";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+import MenuItem from "@mui/material/MenuItem";
+
 type Props = {};
 
 const page = (props: Props) => {
-  // function handleCurrency() {
-  //   var myHeaders = new Headers();
-  //   myHeaders.append("apikey", "qGMMLTHEtOwOsv1NPW5Toh5zgKlBLPfu");
+  const [selected, setSelected] = useState<any>("");
+  const [fromCurrency, setFromCurrency] = useState("INR");
+  const [toCurrency, setToCurrency] = useState("INR");
+  const [fromCurrencyValue, setFromCurrencyValue] = useState<number>(0);
+  const [toCurrencyValue, setToCurrencyValue] = useState<number>(0);
 
-  //   var requestOptions = {
-  //     method: "GET",
-  //     redirect: "follow",
-  //     headers: myHeaders,
-  //   };
+  const handleChangeFrom = (event: SelectChangeEvent) => {
+    setFromCurrencyValue(event.target.value as unknown as number);
+    console.log(event.target.value);
+  };
 
-  //   fetch(
-  //     "https://api.apilayer.com/fixer/latest?symbols=USD%2C%20HKD%2C%20AUD&base=INR",
-  //     requestOptions
-  //   )
-  //     .then((response) => response.text())
-  //     .then((result) => console.log(result))
-  //     .catch((error) => console.log("error", error));
-  // }
+  const handleChangeTo = (event: SelectChangeEvent) => {
+    setToCurrencyValue(event.target.value as unknown as number);
+    console.log(toCurrencyValue);
+  };
+  const handleSelect = (countryCode: any) => {
+    setSelected(countryCode);
+  };
+  const handleCurrency = async () => {
+    try {
+      const myHeaders = new Headers();
+      myHeaders.append("apikey", "qGMMLTHEtOwOsv1NPW5Toh5zgKlBLPfu");
+
+      const requestOptions: RequestInit = {
+        method: "GET",
+        redirect: "follow",
+        headers: myHeaders,
+      };
+
+      const response = await fetch(
+        `https://api.apilayer.com/fixer/latest?symbols=${toCurrency}&base=${fromCurrency}`,
+        requestOptions
+      );
+
+      if (!response.ok) {
+        throw new Error("Request failed with status: " + response.status);
+      }
+
+      const result = await response.text();
+      console.log(result);
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  };
   return (
     <div className="page-container">
       <div className="grid-bg">
@@ -39,20 +71,107 @@ const page = (props: Props) => {
             spacing={2}
           >
             <Grid item>
-              <ConverterInput label="From" />
+              <Box
+                sx={{
+                  border: "2px solid #E0E0E0",
+                  borderRadius: "20px",
+                  padding: 4,
+                  backgroundColor: "#fff",
+                }}
+              >
+                <Stack direction="row" flexWrap="wrap" spacing={8}>
+                  <Stack direction="column" flexWrap="wrap">
+                    <Typography sx={{ color: "gray", fontSize: "14px" }}>
+                      from
+                    </Typography>
+
+                    <TextField
+                      type="number"
+                      variant="standard"
+                      InputProps={{
+                        inputProps: { min: 0 },
+                        disableUnderline: true,
+                      }}
+                    />
+                  </Stack>
+                  <Stack direction="column" flexWrap="wrap">
+                    <div>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        // value={toCurrency}
+                        label="Currency"
+                        onChange={handleChangeFrom}
+                        sx={{
+                          backgroundColor: "#EFF4FE",
+                          color: "#294FE2",
+                          fontWeight: "700",
+                        }}
+                      >
+                        <MenuItem value="USD">USD</MenuItem>
+                        <MenuItem value="INR">INR</MenuItem>
+                        <MenuItem value="HKD">HKD</MenuItem>
+                      </Select>
+                    </div>
+                  </Stack>
+                </Stack>
+              </Box>
             </Grid>
             <Grid item>
               <Image src={Arrow} alt="arrows" height={40} width={40} />
             </Grid>
 
             <Grid item>
-              <ConverterInput label="To" />
+              <Box
+                sx={{
+                  border: "2px solid #E0E0E0",
+                  borderRadius: "20px",
+                  padding: 4,
+                  backgroundColor: "#fff",
+                }}
+              >
+                <Stack direction="row" flexWrap="wrap" spacing={8}>
+                  <Stack direction="column" flexWrap="wrap">
+                    <Typography sx={{ color: "gray", fontSize: "14px" }}>
+                      To
+                    </Typography>
+
+                    <TextField
+                      type="number"
+                      variant="standard"
+                      InputProps={{
+                        inputProps: { min: 0 },
+                        disableUnderline: true,
+                      }}
+                    />
+                  </Stack>
+                  <Stack direction="column" flexWrap="wrap">
+                    <div>
+                      <Select
+                        labelId="demo-simple-select-label"
+                        id="demo-simple-select"
+                        // value={toCurrency}
+                        label="Currency"
+                        onChange={handleChangeTo}
+                        sx={{
+                          backgroundColor: "#EFF4FE",
+                          color: "#294FE2",
+                          fontWeight: "700",
+                        }}
+                      >
+                        <MenuItem value="USD">USD</MenuItem>
+                        <MenuItem value="INR">INR</MenuItem>
+                        <MenuItem value="HKD">HKD</MenuItem>
+                      </Select>
+                    </div>
+                  </Stack>
+                </Stack>
+              </Box>
             </Grid>
           </Grid>
           <Button
             variant="contained"
             id="btn"
-            // onClick={handleCurrency}
             sx={{
               marginTop: 2,
               width: 1 / 5,
